70023591a929c1b0ddb9f5bd663dda0699b09fdd
teghsingh123 <teghsinghabc@gmail.com>
2022-08-29 16:37:40 -0700

    second update

--- app.py
+++ app.py
@@ -39,24 +39,36 @@ def upload():
         file = form.file.data
         file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)),app.config['UPLOAD_FOLDER'], secure_filename("audio.wav")))
         return redirect(url_for("modify"))
+    try:
+        os.remove("static/files/spedupaudio.wav")
+    except OSError:
+        pass
+    try:
+        os.remove("static/files/sloweddownaudio.wav")
+    except OSError:
+        pass
+    try:
+        os.remove("static/files/audio.wav")
+    except OSError:
+        pass
     return render_template("input.html",form=form )
+
+#work on fixing the file not found issue.
 
 @app.route("/modify")
 def modify():
+
     spedup = AudioSegment.from_wav("./static/files/audio.wav")
     spedup = speed_change(spedup, 1.5)
     spedup.export("spedupaudio.wav", format="wav")
     spedup = "spedupaudio.wav"
-    if os.path.exists("static/files\spedupaudio.wav"):
-        os.remove("static/files\spedupaudio.wav")
     shutil.move("spedupaudio.wav", "static/files")
+
 
     sloweddown = AudioSegment.from_wav("./static/files/audio.wav")
     sloweddown = speed_change(sloweddown, 0.75)
     sloweddown.export("sloweddownaudio.wav", format="wav")
     sloweddown = "sloweddownaudio.wav"
-    if os.path.exists("static/files\sloweddownaudio.wav"):
-        os.remove("static/files\sloweddownaudio.wav")
     shutil.move("sloweddownaudio.wav", "static/files")
 
     return render_template("buttons.html")
@@ -64,14 +76,30 @@ def modify():
 @app.route("/spedup")
 def spedup():
     return render_template("spedup.html")
+@app.route('/downloadspedup')
+def downloadspedup():
+    path = "static/files\spedupaudio.wav"
+    return send_file(path, as_attachment=True)
+
+
 
 @app.route("/normal")
 def normal():
     return render_template("normal.html")
+@app.route('/downloadnormal')
+def downloadnormal():
+    path = "static/files\\audio.wav"
+    return send_file(path, as_attachment=True)
+
+
 
 @app.route("/sloweddown")
 def sloweddown():
     return render_template("sloweddown.html")
+@app.route('/downloadsloweddown')
+def downloadsloweddown():
+    path = "static/files\sloweddownaudio.wav"
+    return send_file(path, as_attachment=True)
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ test.py
@@ -0,0 +1,2 @@
+import os
+os.remove("static/files/audio.wav")
